949060cac8a5e418cdb1083ac5d6af6ed41940e3
Avtem <65607782+Avtem@users.noreply.github.com>
2023-02-25 20:55:11 +0100

    Alex: v.1.1.0 reply

--- main.cpp
+++ main.cpp
@@ -4,27 +4,14 @@
 #include <numeric>
 #include <random>
 #include <string>
-
-
-void ignoreLine()
-{
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-char getCharacter()
-{
-    char operation{};
-    std::cin >> operation;
-    ignoreLine(); // remove any extraneous input
-    return operation;
-}
-
+ 
+ 
 class Point
 {
 public:
-    int x;
-    int y;
-
+    int x{};
+    int y{};
+ 
     bool operator==(Point other) const
     {
         return x == other.x && y == other.y;
@@ -35,14 +22,14 @@ public:
     }
     Point operator+(Point other) const
     {
-        return {x + other.x, y + other.y};
+        return { x + other.x, y + other.y };
     }
     Point operator-(Point other) const
     {
-        return {x - other.x, y - other.y};
+        return { x - other.x, y - other.y };
     }
 };
-
+ 
 class Direction
 {
 public:
@@ -52,87 +39,86 @@ public:
         down,
         left,
         right,
-        COUNT,
     };
-
-    Direction(DirectionType type)
+ 
+    explicit Direction(DirectionType type)
         :m_type(type)
     {
     }
-
-    DirectionType getType() const
+ 
+    explicit operator Point() const
     {
-        return m_type;
+        static const std::vector<Point> dirs{ { {0,-1}, {0,1}, {-1,0}, {1,0} } };
+        return dirs[m_type];
     }
-
-    static bool isDirection(char character)
-    {
-        return character == 'w'
-            || character == 'a'
-            || character == 's'
-            || character == 'd';
-    }
-
-    operator Point() const
-    {
-        return m_dirs[m_type];
-    }
+ 
     Direction operator-() const
     {
-        return getOpposite();
-    }
-    Direction getOpposite() const
-    {
-        switch(m_type)
+        switch (m_type)
         {
-            case up:    return Direction{down};
-            case down:  return Direction{up};
-            case left:  return Direction{right};
-            case right: return Direction{left};
+        case up:    return Direction{ down };
+        case down:  return Direction{ up };
+        case left:  return Direction{ right };
+        case right: return Direction{ left };
         }
+ 
         assert(0 && "Unsupported direction was passed!");
-        return Direction{up};
+        return Direction{ up };
     }
-
-    static Direction getDirectionFromUser()
-    {
-        char dir{'0'};
-        while(!isDirection(dir))
-            dir = getCharacter();
-
-        return charToDirection(dir);
-    }
-
-    static Direction getRand()
-    {
-        static std::mt19937 mt(std::random_device{}());
-        return Direction(DirectionType(mt() % DirectionType::COUNT));
-    }
-
+ 
 private:
-///////////////// member variables:
-    DirectionType m_type;
-    // vector that represents up, down, left, right as Points
-    static const std::vector<Point> m_dirs;
-
-    // this function is used only within this class,
-    // so hide it from the public.
-    static Direction charToDirection(char character)
+    DirectionType m_type{};
+ 
+};
+ 
+namespace UserInput
+{
+    static bool isValidCommand(char ch)
     {
-        switch(character)
+        return ch == 'w'
+            || ch == 'a'
+            || ch == 's'
+            || ch == 'd'
+            || ch == 'q';
+    }
+ 
+    void ignoreLine()
+    {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+ 
+    char getCharacter()
+    {
+        char operation{};
+        std::cin >> operation;
+        ignoreLine(); // remove any extraneous input
+        return operation;
+    }
+ 
+    char getCommandFromUser()
+    {
+        char ch{};
+        while (!isValidCommand(ch))
+            ch = getCharacter();
+ 
+        return ch;
+    }
+ 
+    Direction charToDirection(char ch)
+    {
+        switch (ch)
         {
-            case 'w': return Direction{up};
-            case 's': return Direction{down};
-            case 'a': return Direction{left};
-            case 'd': return Direction{right};
+        case 'w': return Direction{ Direction::up };
+        case 's': return Direction{ Direction::down };
+        case 'a': return Direction{ Direction::left };
+        case 'd': return Direction{ Direction::right };
         }
+ 
         assert(0 && "Unsupported direction was passed!");
-        return Direction{up};
+        return Direction{ Direction::up };
     }
 };
-
-const std::vector<Point>Direction::m_dirs = {{0,-1}, {0,1}, {-1,0}, {1,0}};
-
+ 
 class Tile
 {
 public:
@@ -141,14 +127,14 @@ public:
         :m_num(number)
     {
     }
-
+ 
     friend std::ostream& operator<<(std::ostream& stream, Tile tile)
     {
-        if(tile.m_num > 9)
-            stream << " "  << std::to_string(tile.m_num) << " ";
-        else if(tile.m_num > 0)
+        if (tile.m_num > 9)
+            stream << " " << std::to_string(tile.m_num) << " ";
+        else if (tile.m_num > 0)
             stream << "  " << std::to_string(tile.m_num) << " ";
-        else if(tile.m_num == 0)
+        else if (tile.m_num == 0)
             stream << "    ";
         return stream;
     }
@@ -156,30 +142,28 @@ public:
     {
         return m_num == 0;
     }
-
+ 
     int getNum() const { return m_num; }
+ 
 private:
-    int m_num;
+    int m_num{};
 };
-
-
-
-void printEmptyLines(int count)
-{
-    for(int i = 0; i < count; i++)
-        std::cout << '\n';
-}
-
+ 
 class Field
 {
 public:
+ 
     Field()
     {
         createSolvedField();
-        randomizeTiles();
-        draw(); // draw tiles for the first time
     }
-
+ 
+    void printEmptyLines(int count) const
+    {
+        for (int i = 0; i < count; i++)
+            std::cout << '\n';
+    }
+ 
     void draw() const
     {
         // Before drawing always print some empty lines
@@ -190,115 +174,135 @@ public:
         // Increase amount of new lines if your field isn't
         // at the very bottom of the console
         printEmptyLines(25);
-
-        for(int y=0, i=0; y < SIZE; ++y)
+ 
+        for (int y = 0, i = 0; y < SIZE; ++y)
         {
-            for(int x=0; x < SIZE; ++x, ++i)
+            for (int x = 0; x < SIZE; ++x, ++i)
                 std::cout << m_tiles[y][x];
             std::cout << '\n';
         }
     }
-
+ 
     Point getEmptyTilePos()
     {
-        for(int y=0; y < SIZE; ++y)
-            for(int x=0; x < SIZE; ++x)
-                if(m_tiles[y][x].isEmpty())
-                    return {x,y};
-
+        for (int y = 0; y < SIZE; ++y)
+            for (int x = 0; x < SIZE; ++x)
+                if (m_tiles[y][x].isEmpty())
+                    return { x,y };
+ 
         assert(0 && "There is no empty tile in the field!!!");
-        return {-1,-1};
+        return { -1,-1 };
     }
+ 
     // create field that looks like 1,2,3,4,5,6 ... 14,15,0
     void createSolvedField()
     {
-        for(int y = 0, i=1; y < SIZE; ++y)
-            for(int x = 0; x < SIZE; ++x, ++i)
+        for (int y = 0, i = 1; y < SIZE; ++y)
+            for (int x = 0; x < SIZE; ++x, ++i)
                 m_tiles[y][x] = Tile(i);
-
+ 
         // init empty cell
-        m_tiles[SIZE -1][SIZE -1] = Tile(0);
+        m_tiles[SIZE - 1][SIZE - 1] = Tile(0);
     }
-
+ 
     static bool isValidTilePos(Point pt)
     {
         return (pt.x >= 0 && pt.x < SIZE)
             && (pt.y >= 0 && pt.y < SIZE);
     }
-
+ 
     // if adj. point is invalid, it returns the origin
     Point getAdjPoint(const Point& origin, Direction dir)
     {
         Point copy = origin + Point(dir);
-
-        if(isValidTilePos(copy))
+ 
+        if (isValidTilePos(copy))
             return copy;
+ 
         // that adj tile is not valid, so return the origin point
         return origin;
     }
-
+ 
     void swapTiles(Point pt1, Point pt2)
     {
         std::swap(m_tiles[pt1.y][pt1.x], m_tiles[pt2.y][pt2.x]);
     }
-
-    // returns true if user moved successfully
-    bool parseUserInput()
+ 
+    // Compare two fields to see if they are equal
+    friend bool operator==(const Field& f1, const Field& f2)
     {
-        Direction dir = Direction::getDirectionFromUser();
-        Point emptyTile{getEmptyTilePos()};
+        for (int y = 0, i = 1; y < SIZE; ++y)
+            for (int x = 0; x < SIZE; ++x, ++i)
+                if (f1.m_tiles[y][x].getNum() != f2.m_tiles[y][x].getNum())
+                    return false;
+ 
+        return true;
+    }
+ 
+    // returns true if user moved successfully
+    bool moveTiles(Direction dir)
+    {
+        Point emptyTile{ getEmptyTilePos() };
         Point movingTile = getAdjPoint(emptyTile, -dir);
         // we didn't move
-        if(emptyTile == movingTile)
+        if (emptyTile == movingTile)
             return false;
-
+ 
         swapTiles(movingTile, emptyTile);
         return true;
     }
-
+ 
     bool playerWon() const
     {
-        const Tile* currTile = &m_tiles[0][0];
-        for(int i=0; i < SIZE * SIZE -1; ++i, ++currTile)
-            if(currTile->getNum() != i +1)
-                return false;
-        return true;
+        static Field s_solved{};  // generate a solved field
+        return s_solved == *this; // player wins if current field == solved field
     }
-
-    void randomizeTiles()
+ 
+    void randomize()
     {
-        // default way to shuffle tiles
-        // which in 50% will generate an unsolvable tile
-        //std::shuffle(tempCopy.begin(), tempCopy.end(), std::random_device());
-
-        // just move empty tile randomly 1000 times
-        // (just like you would do in real life)
-        for(int i=0; i < 1000; ++i)
-        {
-            Point pt0tile{getEmptyTilePos()};
-            Point ptAdj{pt0tile};
-            // if random direction lead us to nowhere, try again
-            while(pt0tile == ptAdj)
-                ptAdj = getAdjPoint(pt0tile, Direction::getRand());
-
-            swapTiles(pt0tile, ptAdj);
-        }
+        // Create a vector with enough room to hold all numbers in the Field
+        std::vector<int> seq(SIZE * SIZE);
+        // Genenerate an incremental sequence of numbers starting at 0
+        std::iota(seq.begin(), seq.end(), 0);
+        // Randomize that sequence
+        std::shuffle(seq.begin(), seq.end(), std::mt19937{ std::random_device{}() });
+ 
+        // Set our tiles to those numbers
+        for (int y = 0, i = 0; y < SIZE; ++y)
+            for (int x = 0; x < SIZE; ++x, ++i)
+                m_tiles[y][x] = Tile(seq[i]);
     }
-
+ 
 private:
     static const int SIZE = 4;
-    Tile m_tiles[SIZE][SIZE];
+    Tile m_tiles[SIZE][SIZE]{};
 };
-
+ 
 int main()
 {
-    Field field;
-    while(!field.playerWon())
+    Field field{};
+    field.randomize();
+    field.draw();
+ 
+    while (!field.playerWon())
     {
-        bool userMoved = field.parseUserInput();
-        if(userMoved)
+        char ch{ UserInput::getCommandFromUser() };
+ 
+        // Handle non-direction commands
+        if (ch == 'q')
+        {
+            std::cout << "\n\nBye!\n\n";
+            return 0;
+        }
+ 
+        // Handle direction commands
+        Direction dir{ UserInput::charToDirection(ch) };
+ 
+        bool userMoved = field.moveTiles(dir);
+        if (userMoved)
             field.draw();
     }
-
+ 
     std::cout << "\n\nYou won!\n\n";
+    return 0;
 }
